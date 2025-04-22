5f82f80516ba11877f20d1938f1f6c9168f793c7
vaishnavvr13 <rinoykj007@example.com>
2025-04-22 23:11:03 +0100

    test railway env

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
-
+console.log("process.env.BASE_URL", process.env.BASE_URL);
+console.log("process.env.ALLOWED_HOST", process.env.ALLOWED_HOST);
 export const api = axios.create({
   baseURL: `${BASE_URL}/api`,
 });
