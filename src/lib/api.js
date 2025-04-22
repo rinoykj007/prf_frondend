d8601fcfe9deb62b76d9fcbacc975b7efecb59d8
vaishnavvr13 <rinoykj007@example.com>
2025-04-22 23:21:52 +0100

    update api base url

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
-console.log("process.env.BASE_URL", process.env.BASE_URL);
-console.log("process.env.ALLOWED_HOST", process.env.ALLOWED_HOST);
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
 export const api = axios.create({
   baseURL: `${BASE_URL}/api`,
 });
